b096e6f9957d150200da3f16786d92af1a4c71e2
SergioBuzi <sergio.buzi97@gmail.com>
2024-01-28 17:32:36 +0100

    config: setup base scaffolding

---
+++ front-end/src/App.vue
@@ -0,0 +1,19 @@
+<script>
+import AppMain from './components/AppMain.vue';
+
+export default{
+  components:{
+    AppMain,
+  }
+}
+</script>
+
+<template>
+
+  <AppMain />
+
+</template>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ front-end/src/components/AppMain.vue
@@ -0,0 +1,14 @@
+<script>
+export default {
+    name: 'AppMain',
+}
+
+</script>
+
+<template>
+    <h1>ciao mondo</h1>
+</template>
+
+<style scoped>
+
+</style>
